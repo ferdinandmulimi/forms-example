807c1b2d2a519a7ee5b777c2b6b595a1c2c7ca19
Ferdianand.mulimi <ferdinand.mulimi@student.moringaschool.com>
2021-12-13 16:43:45 +0300

    to update Friday lesson on git

--- forms.js
+++ forms.js
@@ -1,10 +1,14 @@
-//submit forms
-$("blanks form").submit(function (event) {
-    var email = $("input#email").val();
-    var password = $("input#password").val();
-    var signin = $("input#radio[name=signin]}")
-    //output
-    $("email").append(email);
-    $("password").append(password);
-    event.preventDefault();
+$(document).ready(function () {
+    //submit, focus, val key word
+    $("#blanks").submit(function () {
+        //input
+        var email = $(".email").val();
+        var password = $(".password").val();
+        var signin= $("input#radio[name=signin]:checked").val();
+        console.log("Remember Me",signin)
+        //output
+        $(".email").append(email);
+        $(".password").append(password);
+       event.preventDefault(); 
+    });
 })
